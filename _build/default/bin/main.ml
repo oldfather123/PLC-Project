36511955c9ae8f11ff370828af77b206082512e9
oldfather123 <Idonotnoliuzhanhe@126.com>
2025-07-25 19:22:56 +0800

    Test

--- _build/default/bin/main.ml
+++ _build/default/bin/main.ml
@@ -3,7 +3,7 @@ open Lib.Lexer
 open Lib.Parser
 open Lib.Transfer
 (* open Lib.Riscv_gen *)
-open Lib.Riscv_gen_new
+(* open Lib.Riscv_gen_new *)
 (* open Lib.Riscv_eval *)
 (* open Lib.Cfg_gen *)
 
@@ -131,7 +131,7 @@ let parse_stdin () =
        | _ -> Printf.printf "Program returned a function\n")
   | exception _ -> Printf.eprintf "No 'main' function found\n" *)
 
-(* let string_of_tac = function
+let string_of_tac = function
   | TacAssign (a, b) -> Printf.sprintf "%s = %s" a b
   | TacBinOp (a, b, op, c) -> Printf.sprintf "%s = %s %s %s" a b op c
   | TacUnOp (a, op, b) -> Printf.sprintf "%s = %s %s" a op b
@@ -146,9 +146,9 @@ let parse_stdin () =
   | TacPhi (p,t,e) -> Printf.sprintf "%s = phi(%s, %s)" p t e
 
 let print_tac tac_list =
-  List.iter (fun tac -> print_endline (string_of_tac tac)) tac_list *)
+  List.iter (fun tac -> print_endline (string_of_tac tac)) tac_list
 
-let print_riscv_inst = function
+(* let print_riscv_inst = function
   | RAdd (rd, rs1, rs2) -> Printf.printf "add %s, %s, %s\n" rd rs1 rs2
   | RSub (rd, rs1, rs2) -> Printf.printf "sub %s, %s, %s\n" rd rs1 rs2
   | RMul (rd, rs1, rs2) -> Printf.printf "mul %s, %s, %s\n" rd rs1 rs2
@@ -179,7 +179,7 @@ let print_riscv_inst = function
   | RBge (r1, r2, label) -> Printf.printf "bge %s, %s, %s\n" r1 r2 label
 
 let print_riscv riscv_list =
-  List.iter print_riscv_inst riscv_list
+  List.iter print_riscv_inst riscv_list *)
 
 (* let print_cfg cfg =
   Hashtbl.iter (fun label block ->
@@ -199,13 +199,13 @@ let () =
     (* print_comp_unit ast; *)
     
     let tac_list = gen_comp_unit ast in
-    (* Printf.printf "==Original TAC==\n";
+    Printf.printf "==Original TAC==\n";
     print_tac tac_list;
 
-    Printf.printf "==Original RISC-V Code==\n"; *)
+    (* Printf.printf "==Original RISC-V Code==\n";
     let riscv_list = tac_to_riscv tac_list in
     Printf.printf ".global main\n";
-    print_riscv (riscv_list);
+    print_riscv (riscv_list); *)
 
     (* let (cfg, _blk) = build_cfg tac_list in
     Printf.printf "==Original CFG==\n";
--- test/test17.c
+++ test/test17.c
@@ -1,7 +1,18 @@
+// int main () {
+//     int x = 5;
+//     while (x != 0) {
+//         x = x - 1;
+//     }
+//     int y = 3;
+//     x = y;
+//     return x;
+// }
 int main () {
-    int x = 5;
-    while (x != 0) {
-        x = x - 1;
-    }
-    return x;
+    int a = 1;
+    int b = 2;
+    int c = 3;
+    a = 2;
+    a = c + 1;
+    b = b + a;
+    return b;
 }
